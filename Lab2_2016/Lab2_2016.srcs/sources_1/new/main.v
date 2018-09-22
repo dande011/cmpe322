2c5955b4e7b1b65948f2f506578dea94cc088f9b
David <jonie4@gmail.com>
2018-09-21 17:21:48 -0700

    Revert

--- Lab2_2016/Lab2_2016.srcs/sources_1/new/main.v
+++ Lab2_2016/Lab2_2016.srcs/sources_1/new/main.v
@@ -22,7 +22,7 @@
 
 module main(
     output reg [6:0] seg,
-    output dp,
+//    output dp,
     output reg [3:0] an,
     input btnU,
     input btnD,
@@ -35,27 +35,17 @@ module main(
     `include "hexToSeg_funct.vh"
     `include "math_funct.vh"
     
-<<<<<<< HEAD
-=======
-    
-    reg [1:0] test = 0;
-    
-    
->>>>>>> parent of b5b4bc7... Heading to meeting
+    reg reset = 0;
     reg [16:0] output_value = 0;
-    wire [16:0] input_value = 0;
+    reg [16:0] input_value = 0;
     reg [19:0] refresh_counter = 0;
     wire [1:0] activating_counter;
                    //  count   0  ->  1  ->  2  ->  3
                    // annode LED1   LED2   LED3   LED4
-<<<<<<< HEAD
-=======
     //Array of 5 buttons
     // U D L R C
-    wire btn [4:0] = {btnU, btnD, btnL, btnR, btnC};
->>>>>>> parent of b5b4bc7... Heading to meeting
+    wire btn [4:0] = {btnC, btnR, btnL, btnD, btnU};
     //Array of 5 pressed variables
-    // U D L R C
     reg [19:0] pressed [4:0];
     
     //debounce 5000 cycles => 1/20000 second
@@ -64,27 +54,26 @@ module main(
     //refresh_counter
     always @(posedge CLK100MHZ)
     begin
-<<<<<<< HEAD
-        if(btnC && !pressed[4])begin
+        if(!btn[4] && !pressed[4])begin
             refresh_counter <= 0;
             pressed[4] <= refresh_counter;
+            reset <= 1;
         end
-        else if(btnC && (refresh_counter - pressed[4] >= DEBOUNCE))begin
-=======
-        if(btn[4] == 1 && pressed[4] == 0)begin
-            refresh_counter <= 0;
-            pressed[4] <= refresh_counter;
-        end
-        else if(btn[4] == 0 && (refresh_counter - pressed[4] >= DEBOUNCE))
->>>>>>> parent of b5b4bc7... Heading to meeting
+        else if(btn[4] && (refresh_counter - pressed[4] >= DEBOUNCE))begin
             pressed[4] <= 0;
-        else
-            refresh_counter <= refresh_counter + 1;
+        end
+        refresh_counter <= refresh_counter + 1;
     end //refresh_counter
     assign activating_counter = refresh_counter[19:18];
     
     
+    reg [3:0] counter = 0;
     //Set new value from switches
+    always @(posedge CLK100MHZ)
+    begin
+        for(counter = 0; counter <= 4'b111; counter = counter + 1)
+            input_value[counter] = sw[counter];
+    end //Set from switches
 //    generate
 //    genvar counter;
 //        for(counter = 0; counter <= 4'b1111; counter = counter + 1) begin : counter_outer
@@ -92,31 +81,20 @@ module main(
 //        end
 //    endgenerate
     
-    
+    reg [1:0] i;
     //Check Buttons
-<<<<<<< HEAD
     always @(posedge CLK100MHZ)
     begin
         for(i=0; i < 2'b11; i = i + 1)
         begin
-            if(btnC)
+            if(reset)
                 output_value <= 0;
-            if(btnU && !pressed[i])
-=======
-    generate
-        genvar i;
-        for(i = 0; i <= 3; i = i + 1) begin : btn_outer
-            always @(posedge CLK100MHZ) begin
-            if(btn[i] == 1 && pressed[i] == 0)
-                pressed[i] <= refresh_counter;
-            else if(btn[i] == 0 && (refresh_counter - pressed[i] >= DEBOUNCE))
->>>>>>> parent of b5b4bc7... Heading to meeting
+            if(btn[i] && !pressed[i])
                 begin
-                    pressed[i] <= 0;
-                    output_value <= math_funct(i,output_value,sw);
+                    pressed[i] <= refresh_counter;
+                    output_value <= math_funct(i,output_value,input_value);
                 end
-<<<<<<< HEAD
-            else if(!btnU && (refresh_counter - pressed[i] >= DEBOUNCE))
+            else if(!btn[i] && (refresh_counter - pressed[i] >= DEBOUNCE))
                 pressed[i] <= 0;
         end
     end //check buttons
@@ -134,11 +112,6 @@ module main(
 //            end
 //        end //btn_outer
 //    endgenerate //Check Buttons
-=======
-            end
-        end //btn_outer
-    endgenerate //Check Buttons
->>>>>>> parent of b5b4bc7... Heading to meeting
     
     
     //Display current values to output
